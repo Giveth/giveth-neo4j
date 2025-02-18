726e6277d5e8c0faa2842db74092db74d4595c3d
Ramin <raminramazanpour@gmail.com>
2025-02-18 18:42:03 +0330

    add api_key_usage table

--- src/add_api_key.py
+++ src/add_api_key.py
@@ -7,6 +7,7 @@ db_path = os.path.join(base_dir, '..', 'data', 'local_data.db')
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
+# Create api_keys table to store API keys
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS api_keys (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,9 +16,21 @@ CREATE TABLE IF NOT EXISTS api_keys (
 )
 ''')
 
+# Add user API key
 cursor.execute('''
 INSERT INTO api_keys (user, api_key) VALUES (?, ?)
 ''', ('sample_name', 'sample_unique_api_key_12345')) # Replace with your desired info
+
+# Create a new table for tracking API key usage
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS api_key_usage (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    api_key TEXT NOT NULL,
+    endpoint TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    request_body TEXT
+)
+''')
 
 conn.commit()
 conn.close()
